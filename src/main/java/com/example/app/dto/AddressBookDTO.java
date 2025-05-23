7231037c8f485cceec24a0705811be0b4850c65a
RajeshK1006 <rajesh.kofficialmail@gmail.com>
2025-05-23 21:44:28 +0530

    UC6: Used Lombok in AddressBookDTO to auto-generate boilerplate code

--- src/main/java/com/example/app/dto/AddressBookDTO.java
+++ src/main/java/com/example/app/dto/AddressBookDTO.java
@@ -1,40 +1,14 @@
 package com.example.app.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressBookDTO {
     private String name;
     private String address;
     private String phone;
-
-    public AddressBookDTO() {}
-
-    public AddressBookDTO(String name, String address, String phone) {
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-    }
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
